592e857a29144ce09050a30376998f0794e78799
Gustavo Sandoval <gussand@gmail.com>
2016-04-18 00:17:31 -0400

    fix to have bernie as the main route

--- incubator.py
+++ incubator.py
@@ -3,20 +3,12 @@ from flask import request, render_template
 
 app = Flask(__name__)
 
-@app.route("/api/images/bernietwitter", methods=['GET'])
-def bernietwitter():
-	app.logger.debug('Entered images_leaderboard. Payload: ' )
-	app.logger.debug(request.get_json())
 
-
+@app.route('/')
 @app.route('/bernie')
 def berniebills():
     return render_template('bernie.html')
 
-
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
 
 if __name__ == '__main__':
     #app.run(host='0.0.0.0', port=8080, debug=True)
